19117868366e5f1f1dd855bee3bc836b29c48922
Costin STROIE <cstroie@bitdefender.com>
2018-01-23 18:02:16 +0200

    Fonts.

---
+++ fntbld.h
@@ -0,0 +1,28 @@
+/**
+  fntbld.h - Bold 5x8 font
+
+  Copyright (c) 2017-2018 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of LedMatrixClock.
+
+  https://xantorohara.github.io/led-matrix-editor/#f060606060607060|f8183060c0c0c870|70c8c0c060c0c870|c0c0c0c0f8c8c8c8|70c8c0c0781818f8|7098989878189870|3030303060c0c0f8|70c8c8c870c8c870|70c8c0f0c8c8c870|70c8c8c8c8c8c870
+*/
+
+#ifndef FNTBLD_H
+#define FNTBLD_H
+
+const uint64_t IMAGES[] PROGMEM = {
+  0x70c8c8c8c8c8c870,
+  0xf060606060607060,
+  0xf8183060c0c0c870,
+  0x70c8c0c060c0c870,
+  0xc0c0c0c0f8c8c8c8,
+  0x70c8c0c0781818f8,
+  0x7098989878189870,
+  0x3030303060c0c0f8,
+  0x70c8c8c870c8c870,
+  0x70c8c0f0c8c8c870,
+};
+const int IMAGES_LEN = sizeof(IMAGES) / 8;
+
+#endif /* FNTBLD_H */
---
+++ fntsqr.h
@@ -0,0 +1,28 @@
+/**
+  fntsqr.h - Rectangular 5x5 font
+
+  Copyright (c) 2017-2018 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of LedMatrixClock.
+
+  https://xantorohara.github.io/led-matrix-editor/#00f898a8c8f80000|00e0404040600000|00f808f880f80000|00f880f080f80000|008080f888880000|00f880f808f80000|00f888f808f80000|0080808088f80000|00f888f888f80000|00f880f888f80000
+*/
+
+#ifndef FNTSQR_H
+#define FNTSQR_H
+
+const uint64_t IMAGES[] = {
+  0x00f898a8c8f80000,
+  0x00e0404040600000,
+  0x00f808f880f80000,
+  0x00f880f080f80000,
+  0x008080f888880000,
+  0x00f880f808f80000,
+  0x00f888f808f80000,
+  0x0080808088f80000,
+  0x00f888f888f80000,
+  0x00f880f888f80000
+};
+const int IMAGES_LEN = sizeof(IMAGES) / 8;
+
+#endif /* FNTSQR_H */
