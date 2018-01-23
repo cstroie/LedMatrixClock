4279b60b834a8df7f9f896f6c48181e18071378c
Costin STROIE <cstroie@bitdefender.com>
2018-01-23 18:02:30 +0200

    Added support for RTC.

--- LedMatrixClock.ino
+++ LedMatrixClock.ino
@@ -1,68 +1,116 @@
-#include <LedControl.h>
+/**
+  LedMatrixClock - A desk clock using 3 8x8 led matrix modules
 
+  Copyright 2017-2018 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of LedMatrix Clock.
+
+  LedMatrixClock is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  LedMatrixClock is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  LedMatrixClock.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <LedControl.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+// Pin definitions
 const int DIN_PIN = 11;
-const int CS_PIN = 13;
+const int CS_PIN  = 13;
 const int CLK_PIN = 12;
 
-const uint64_t IMAGES[] PROGMEM = {
-  0x70c8c8c8c8c8c870,
-  0xf060606060607060,
-  0xf8183060c0c0c870,
-  0x70c8c0c060c0c870,
-  0xc0c0c0c0f8c8c8c8,
-  0x70c8c0c0781818f8,
-  0x7098989878189870,
-  0x3030303060c0c0f8,
-  0x70c8c8c870c8c870,
-  0x70c8c0f0c8c8c870,
-};
-const int IMAGES_LEN = sizeof(IMAGES) / 8;
+// Define the digits
+//#include "fntrct.h"
+//#include "fntsqr.h"
+//#include "fntstd.h"
+#include "fntbld.h"
+
+// The RTC
+RTC_DS3231 rtc;
+//RTC_Millis rtc;
 
 
-LedControl display = LedControl(DIN_PIN, CLK_PIN, CS_PIN, 3);
+// The matrix object
+LedControl mtx = LedControl(DIN_PIN, CLK_PIN, CS_PIN, 3);
 
-void show(uint8_t hh, uint8_t mm) {
+uint8_t mtxBrightness = 2;
+
+/**
+  Show the time
+
+  @param hh hours   0..23
+  @param mm minutes 0..59
+*/
+void mtxShowTime(uint8_t hh, uint8_t mm) {
+  // Decompose to 4 digits
   uint8_t tm[4];
   tm[0] = hh / 10; tm[1] = hh % 10;
   tm[2] = mm / 10; tm[3] = mm % 10;
 
+  // Create a RAM copy of the font (only the used digits)
   uint8_t digit[4][8];
   for (int i = 0; i < 4; i++)
     memcpy_P(&digit[i], &IMAGES[tm[i]], 8);
 
+  // Display the columns (the matrices are transposed)
   for (int i = 0; i < 8; i++) {
     byte row;
-
+    // First matrix
     row = digit[0][i] >> 3 | digit[1][i] << 3;
-    display.setColumn(0, i, row);
-
+    mtx.setColumn(0, i, row);
+    // Second matrix
     row = digit[1][i] >> 5 | digit[2][i] << 2;
-    display.setColumn(1, i, row);
-
+    mtx.setColumn(1, i, row);
+    // Third matrix
     row = digit[2][i] >> 6 | digit[3][i];
-    display.setColumn(2, i, row);
+    mtx.setColumn(2, i, row);
   }
 }
 
+/**
+  Main Arduino setup function
+*/
 void setup() {
+  // Init the serial com
   Serial.begin(9600);
 
-  //we have already set the number of devices when we created the LedControl
-  int devices = display.getDeviceCount();
-  //we have to init all devices in a loop
-  for (int address = 0; address < devices; address++) {
-    /*The MAX72XX is in power-saving mode on startup*/
-    display.shutdown(address, false);
-    /* Set the brightness to a medium values */
-    display.setIntensity(address, 2);
-    /* and clear the display */
-    display.clearDisplay(address);
+  // Init all led matrices in a loop
+  for (int address = 0; address < mtx.getDeviceCount(); address++) {
+    // The MAX72XX is in power-saving mode on startup
+    mtx.shutdown(address, false);
+    // Set the brightness
+    mtx.setIntensity(address, mtxBrightness);
+    // Clear the display
+    mtx.clearDisplay(address);
   }
 
-  show(14, 06);
+  // Init and configure RTC
+  if (! rtc.begin()) {
+    Serial.println(F("Couldn't find RTC"));
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println(F("RTC lost power, lets set the time!"));
+    // The next line sets the RTC to the date & time this sketch was compiled
+    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 }
 
 
 void loop() {
-
+  DateTime now = rtc.now();
+  Serial.println(now.unixtime());
+  mtxShowTime(now.hour(), now.minute());
 }
