353fb8874e64292953a93adbb17a38f2d86a26f9
Costin STROIE <cstroie@bitdefender.com>
2018-01-25 17:49:53 +0200

    Use custom RTC. Do not update if not needed.

--- LedMatrixClock.ino
+++ LedMatrixClock.ino
@@ -20,8 +20,7 @@
 */
 
 #include <LedControl.h>
-#include <Wire.h>
-#include <RTClib.h>
+#include "DS3231.h"
 
 // Pin definitions
 const int DIN_PIN = 11;
@@ -35,14 +34,24 @@ const int CLK_PIN = 12;
 #include "fntbld.h"
 
 // The RTC
-RTC_DS3231 rtc;
-//RTC_Millis rtc;
-
+DS3231 rtc;
 
 // The matrix object
 LedControl mtx = LedControl(DIN_PIN, CLK_PIN, CS_PIN, 3);
+// The font
+uint8_t DIGIT[16][8] = {0};
 
 uint8_t mtxBrightness = 2;
+
+/**
+  Load the specified font into RAM
+
+  @param font the font id
+*/
+void loadFont(uint8_t font) {
+  for (int i = 0; i < IMAGES_LEN; i++)
+    memcpy_P(&DIGIT[i], &IMAGES[i], 8);
+}
 
 /**
   Show the time
@@ -51,29 +60,38 @@ uint8_t mtxBrightness = 2;
   @param mm minutes 0..59
 */
 void mtxShowTime(uint8_t hh, uint8_t mm) {
-  // Decompose to 4 digits
-  uint8_t tm[4];
-  tm[0] = hh / 10; tm[1] = hh % 10;
-  tm[2] = mm / 10; tm[3] = mm % 10;
+  // Keep previous values for hours and minutes
+  static uint8_t _hh = 255, _mm = 255;
 
-  // Create a RAM copy of the font (only the used digits)
-  uint8_t digit[4][8];
-  for (int i = 0; i < 4; i++)
-    memcpy_P(&digit[i], &IMAGES[tm[i]], 8);
+  // Skip if not changed
+  if ((hh == _hh) and (mm == _mm)) return;
+
+  // Decompose hhmm to 4 digits
+  uint8_t tm[4] = {hh / 10, hh % 10, mm / 10, mm % 10};
 
   // Display the columns (the matrices are transposed)
   for (int i = 0; i < 8; i++) {
-    byte row;
+    uint8_t row;
     // First matrix
-    row = digit[0][i] >> 3 | digit[1][i] << 3;
-    mtx.setColumn(0, i, row);
+    if (hh != _hh) {
+      row = DIGIT[tm[0]][i] >> 3 | DIGIT[tm[1]][i] << 3;
+      mtx.setColumn(0, i, row);
+    }
     // Second matrix
-    row = digit[1][i] >> 5 | digit[2][i] << 2;
-    mtx.setColumn(1, i, row);
+    if ((hh != _hh) or (mm != _mm)) {
+      row = DIGIT[tm[1]][i] >> 5 | DIGIT[tm[2]][i] << 2;
+      mtx.setColumn(1, i, row);
+    }
     // Third matrix
-    row = digit[2][i] >> 6 | digit[3][i];
-    mtx.setColumn(2, i, row);
+    if (mm != _mm) {
+      row = DIGIT[tm[2]][i] >> 6 | DIGIT[tm[3]][i];
+      mtx.setColumn(2, i, row);
+    }
   }
+
+  // Keep the current values
+  _hh = hh;
+  _mm = mm;
 }
 
 /**
@@ -92,25 +110,30 @@ void setup() {
     // Clear the display
     mtx.clearDisplay(address);
   }
+  // Load the font
+  loadFont(0);
 
   // Init and configure RTC
-  if (! rtc.begin()) {
-    Serial.println(F("Couldn't find RTC"));
+  if (! rtc.init()) {
+    Serial.println(F("DS3231 RTC missing"));
   }
 
-  if (rtc.lostPower()) {
-    Serial.println(F("RTC lost power, lets set the time!"));
-    // The next line sets the RTC to the date & time this sketch was compiled
-    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+  /*
+    if (rtc.lostPower()) {
+      Serial.println(F("RTC lost power, lets set the time!"));
+      // The next line sets the RTC to the date & time this sketch was compiled
+      //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+      // This line sets the RTC with an explicit date & time, for example to set
+      // January 21, 2014 at 3am you would call:
+      // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    }
+  */
 }
 
-
+/**
+  Main Arduino loop
+*/
 void loop() {
-  DateTime now = rtc.now();
-  Serial.println(now.unixtime());
-  mtxShowTime(now.hour(), now.minute());
+  rtc.readTime();
+  mtxShowTime(rtc.HH, rtc.MM);
 }
