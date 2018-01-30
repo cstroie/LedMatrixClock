ba271bf87336d66bafeaa322b966ad4954095466
Costin STROIE <cstroie@bitdefender.com>
2018-01-30 16:18:49 +0200

    Added some more fonts.

---
+++ fntltw.h
@@ -0,0 +1,28 @@
+/**
+  fntltw.h - Lucida TypeWriter font
+
+  Copyright (c) 2017-2018 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of LedMatrixClock.
+
+  https://xantorohara.github.io/led-matrix-editor/#0070d8d8d8d8d870|0060606060607860|00f8183060c0d870|0070d8c070c0d870|006060f868687060|0070d8c0781818f8|0070d8d87818d870|003030604080f8f8|0070d8d878d8d870|0070d8c0f0d8d870
+*/
+
+#ifndef FNTLTW_H
+#define FNTLTW_H
+
+const uint64_t IMAGES[] PROGMEM = {
+  0x0070d8d8d8d8d870,
+  0x0060606060607860,
+  0x00f8183060c0d870,
+  0x0070d8c070c0d870,
+  0x006060f868687060,
+  0x0070d8c0781818f8,
+  0x0070d8d87818d870,
+  0x003030604080f8f8,
+  0x0070d8d870d8d870,
+  0x0070d8c0f0d8d870
+};
+const int IMAGES_LEN = sizeof(IMAGES)/8;
+
+#endif /* FNTLTW_H */
