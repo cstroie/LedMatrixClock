39bb5388b4c3e8d0f7305de809a4c9425c8ef1e7
Costin STROIE <costinstroie@gmail.com>
2018-02-18 23:53:18 +0200

    Reorganized the fonts. Added some more chars.

--- fonts.h
+++ fonts.h
@@ -13,20 +13,18 @@
 #include "fntbbt.h"
 #include "fntbld.h"
 #include "fntcln.h"
-#include "fntcpm.h"
 #include "fnthel.h"
 #include "fntltw.h"
 #include "fntncs.h"
-#include "fntrct.h"
 #include "fntskd.h"
 #include "fntsqr.h"
 #include "fntstd.h"
 
-const uint64_t* const FONTS[] = {FNTBBT, FNTBLD, FNTCLN, FNTCPM,
-                                 FNTHEL, FNTLTW, FNTNCS, FNTRCT,
-                                 FNTSKD, FNTSQR, FNTSTD,
+const uint64_t* const FONTS[] = {FNTSTD, FNTBBT, FNTSKD,
+                                 FNTNCS, FNTBLD, FNTLTW,
+                                 FNTSQR, FNTHEL, FNTCLN,
                                 };
-uint8_t fontSize  = 10;
-uint8_t fontCount = 11;
+uint8_t fontSize  = 16;
+uint8_t fontCount =  9;
 
 #endif /* FONTS_H */
